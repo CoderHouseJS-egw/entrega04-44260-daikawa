c5e0e66a4327a549f02229e192a3c1f86695da74
Erica Daikawa <erica.daikawa@quintoandar.com.br>
2023-02-20 16:55:12 -0300

    feat: adiciona classe Pessoa com construtor de objetos e métodos

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 /**
  * Projeto Integrador: Simulador Interativo
- * Estrutura Inicial
+ * Incorporar objetos
  * Turma 44260 Javascript
  * Erica Daikawa
  */
@@ -9,59 +9,115 @@
 alert(
   "Controle para Marombeiros\nAplicativo para controle de refeições e exercícios"
 );
-alert("Etapas:\nCadastro pessoal\nCadastro da refeição ou do treino");
+let welcome = prompt("Você já tem cadastro? s/n");
 
-let nome = prompt("Seu nome: ");
-let idade = prompt("Sua idade: ");
-let intIdade = parseInt(idade);
-let altura = parseInt(prompt("Sua altura em centímetros: "));
-let peso = parseFloat(prompt("Seu peso: ")).toFixed(3);
-let goalCalorias = parseInt(
-  prompt("Digite o seu objetivo diário de calorias: ")
-);
-let goalTreino = parseInt(
-  prompt("Digite o seu objetivo de treino semanal em dias: ")
-);
+let marcelo = {
+  username: "Marcelo",
+  bornDate: "1972/02/14",
+  height: 172,
+  weight: 72,
+  calorieGoal: 1700,
+  trainingGoal: 5,
+};
+let pessoa0 = {};
+let pessoa1 = {};
 
-alert(
-  "Nome: " +
-    nome +
-    "\nIdade: " +
-    idade +
-    "\nAltura: " +
-    altura +
-    "\nPeso: " +
-    peso +
-    "\nCalorias Diárias: " +
-    goalCalorias +
-    "\nTreinos Semanais: " +
-    goalTreino +
-    "\n"
-);
-let refeicao = parseInt(prompt("Quantas calorias vc comeu na sua refeição? "));
-let treino = prompt("Você treinou hoje? (s/n): ");
-let qtTreino = 0;
-if (treino == "s") {
-  qtTreino = +1;
-} else {
-  qtTreino = +0;
+/** primeiro construtor */
+class Pessoa {
+  constructor(user) {
+    this.username = user.username;
+    // this.age = user.age; // trocar para data nasc e fazer a conta usando Date
+    this.bornDate = user.bornDate;
+    this.height = user.height;
+    this.weight = user.weight;
+    this.calorieGoal = user.calorieGoal;
+    this.trainingGoal = user.trainingGoal;
+  }
+  /** método age - calcula idade */
+  age() {
+    const today = new Date();
+    const birthDate = new Date(this.bornDate);
+    let result = today.getFullYear() - birthDate.getFullYear();
+    const month = today.getMonth() - birthDate.getMonth();
+
+    if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
+      result--;
+    }
+    return result;
+  }
+  /** método insertData - insere refeição e treino */
+  insertData() {
+    let refeicao = parseInt(
+      prompt("Quantas calorias vc comeu na sua refeição? ")
+    );
+    let treino = prompt("Você treinou hoje? (s/n): ");
+    let qtTreino = 0;
+    if (treino == "s") {
+      qtTreino = +1;
+    } else {
+      qtTreino = +0;
+    }
+    /** primeira função */
+    const subtrai = (goal, hoje) => goal - hoje;
+
+    let restaCalorias = subtrai(this.calorieGoal, refeicao);
+    let restaTreino = subtrai(this.trainingGoal, qtTreino);
+
+    alert(
+      this.username +
+        ", você ainda precisa comer " +
+        restaCalorias +
+        " calorias hoje.\nFaltam " +
+        restaTreino +
+        " treinos essa semana."
+    );
+  }
 }
-/** primeira função */
-const subtrai = (goal, hoje) => goal - hoje;
+/** função de login/cadastro */
+function login(welcome) {
+  let cadastro = {};
 
-let restaCalorias = subtrai(goalCalorias, refeicao);
-let restaTreino = subtrai(goalTreino, qtTreino);
+  if (welcome === "s") {
+    pessoa0 = new Pessoa(marcelo);
+    alert("Bem-vindo, " + pessoa0.username);
+    pessoa0.insertData();
+  } else {
+    cadastro = {
+      username: prompt("Seu nome: "),
+      bornDate: prompt("Digite sua data de nascimento no formato YYYY/MM/DD"),
+      height: parseInt(prompt("Sua altura em centímetros: ")),
+      weight: parseFloat(prompt("Seu peso: ")).toFixed(3),
+      calorieGoal: parseInt(
+        prompt("Digite o seu objetivo diário de calorias: ")
+      ),
+      trainingGoal: parseInt(
+        prompt("Digite o seu objetivo de treino semanal em dias: ")
+      ),
+    };
+    pessoa1 = new Pessoa(cadastro);
+    alert(
+      "Nome: " +
+        pessoa1.username +
+        "\nIdade: " +
+        pessoa1.age() +
+        "\nAltura: " +
+        pessoa1.height +
+        "\nPeso: " +
+        pessoa1.weight +
+        "\nCalorias Diárias: " +
+        pessoa1.calorieGoal +
+        "\nTreinos Semanais: " +
+        pessoa1.trainingGoal +
+        "\n"
+    );
+    pessoa1.insertData();
+  }
+}
 
-alert(
-  nome +
-    ", você ainda precisa comer " +
-    restaCalorias +
-    " calorias hoje.\nFaltam " +
-    restaTreino +
-    " treinos essa semana."
-);
+/** primeira chamada da função */
+login(welcome);
 
-/** função: recarregarAPagina */
+/** função: recarregarAPagina (botão na navbar) */
 const recarregarAPagina = () => {
   window.location.reload();
 };
